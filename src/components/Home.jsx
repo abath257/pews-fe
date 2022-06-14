d2570784cd3cf39ef9d0e63a6662d590312fcc43
abath257 <abath257@gmail.com>
2022-06-14 11:27:24 +0100

    Home page now displaying all articles, styling added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,37 @@
 import { useState, useEffect } from "react";
+import { getArticles } from "../utils/api";
 
 const Home = () => {
-const [articles,setArticles] = useState([
-    {
-      "title": "Seafood substitutions are increasing",
-      "topic": "cooking",
-      "author": "weegembump",
-      "body": "Text from the article..",
-      "created_at": "2020-07-09T20:11:00.000Z"
-    }
-  ])
+const [articles,setArticles] = useState([])
+const [isLoading, setIsLoading] = useState(true);
 
 useEffect(()=>{
+getArticles().then((articles)=>{
+    console.log(articles)
+setArticles(articles)
+setIsLoading(false)
+})},[])
 
-})
 
 
+
+if (isLoading === true) return(',.....loading')
     return (
-    <h1>Hello</h1> 
+   <>
+     <h2 id = "articles__header">Positive news articles</h2> 
+   <ul id = "articlesList">
+    {articles.map((article)=>{
+return(
+    <li key={article.article_id} id = "article">
+     <span id = "Home__article--title"><h3>{article.topic}</h3> <h2>{article.title}</h2></span>
+     <h3>{article.author}</h3>
+     <p>{article.body}</p>
+     <h3>{article.created_at}</h3>
+    </li>
+)
+    })}    
+    </ul>
+    </>
     );
 };
 
