6286b6a42273a2cb4aa40f59b8cdd3d608e60b08
abath257 <abath257@gmail.com>
2022-06-16 12:36:52 +0100

    #8 Extracted CommentsCard from Comments, Implemented conditional button to show comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,33 +1,29 @@
 import { useState, useEffect } from "react";
 import { getCommentsById } from "../utils/api";
+import CommentsCard from "./commentsCard";
 
-const Comments = ({comment_count ,article_id}) => {
-const [comments, setComments] = useState( [
-    {
-      "comment_id": 3,
-      "body": "some text here...",
-      "votes": 100,
-      "author": "Mitch",
-      "created_at": "2020-07-09T20:11:00.000Z"
-    }
-  ])
+const Comments = ({ comment_count, article_id }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen((currOpen) => !currOpen);
+  const [comments, setComments] = useState([]);
 
-  useEffect(()=>{
-    getCommentsById(article_id).then((comments)=>{
-    setComments(comments)
-    })},[])
+  useEffect(() => {
+    getCommentsById(article_id).then((comments) => {
+      setComments(comments);
+    });
+  }, []);
 
-    return (
-        <>
-        <section id = "Comments">
-   <h3>Comments: {comment_count}</h3>
-   <ul className="comments__list">
-   {comments.map((comments)=>{return <li key = {comments.comment_id}> <span className ="Comments__main"><h3 className = "Comments__body">{comments.body}</h3><h3 className="Comments.author">{comments.author}</h3></span>
-   <span className ="Comments__footer"><h4>{comments.votes} Votes</h4><h4>{comments.created_at}</h4></span></li>})}
-   </ul> 
-   </section>
-   </>
-    );
+  return (
+    <>
+      <section id="Comments">
+        <h3>Comments: {comment_count}</h3>
+        <button onClick={toggleOpen}>
+          {isOpen ? "No comments" : "See comments"}
+        </button>
+        {isOpen && <CommentsCard comments={comments} />}
+      </section>
+    </>
+  );
 };
 
 export default Comments;
