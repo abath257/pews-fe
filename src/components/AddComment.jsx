3eb201ecc3fced6376d200d6c23b3a27762060d3
abath257 <abath257@gmail.com>
2022-06-17 14:07:00 +0100

    #9 Reverted to previous method of making post request, an error means that nothing will be added

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,41 +1,43 @@
-import { useState, useContext} from "react";
+import { useState, useContext } from "react";
 import { UserContext } from "../contexts/User";
 import { postComment } from "../utils/api";
 
-
 const AddComment = ({ comments, setComments, article_id }) => {
   const [newComment, setNewComment] = useState("");
-  const  user  = useContext(UserContext);
+  const user = useContext(UserContext);
 
-  const handleSubmit = (event) =>{
-    event.preventDefault()
-    postComment(article_id,user,newComment).then((commentFromApi)=>{
-    const newCommentObj = {body: commentFromApi.body,votes:commentFromApi.votes,author:commentFromApi.author,created_at:commentFromApi.created_at} 
-    setComments((currComments)=>{return[newCommentObj, ...currComments]}) 
-    setNewComment('')
-    })}
-
-
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postComment(article_id, user, newComment)
+      .then((commentFromApi) => {
+        const newCommentObj = {
+          body: commentFromApi.body,
+          votes: commentFromApi.votes,
+          author: commentFromApi.author,
+          created_at: commentFromApi.created_at,
+        };
+        setComments((currComments) => {
+          return [...currComments, newCommentObj];
+        });
+      })
+        setNewComment("");
+     
+  };
 
   return (
-      <form onSubmit={handleSubmit}>
-<label>
-    Post a new comment...
-    <input
-    type="text"
-    name="new-comment"
-    value={newComment}
-    onChange={(event) => setNewComment(event.target.value)}
-    />
-</label>
-<button type = 'submit'>Post comment</button>
-      </form>
-  
+    <form onSubmit={handleSubmit}>
+      <label>
+        Post a new comment...
+        <input
+          type="text"
+          name="new-comment"
+          value={newComment}
+          onChange={(event) => setNewComment(event.target.value)}
+        />
+      </label>
+      <button type="submit">Post comment</button>
+    </form>
   );
-  }
+};
 
 export default AddComment;
-
-
-
