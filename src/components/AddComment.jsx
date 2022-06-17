93e2c1b614a85cc54c4e620f6254809694b4217f
abath257 <abath257@gmail.com>
2022-06-17 14:45:10 +0100

    #9 Have added state... hasPosted to add comment to prevent a user reposting an article during the api call

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -5,39 +5,45 @@ import { postComment } from "../utils/api";
 const AddComment = ({ comments, setComments, article_id }) => {
   const [newComment, setNewComment] = useState("");
   const user = useContext(UserContext);
+  const [hasPosted, setHasPosted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    postComment(article_id, user, newComment)
-      .then((commentFromApi) => {
-        const newCommentObj = {
-          body: commentFromApi.body,
-          votes: commentFromApi.votes,
-          author: commentFromApi.author,
-          created_at: commentFromApi.created_at,
-        };
-        setComments((currComments) => {
-          return [...currComments, newCommentObj];
-        });
-      })
-        setNewComment("");
-     
-  };
+    setHasPosted(true);
+    postComment(article_id, user, newComment).then((commentFromApi) => {
+      const newCommentObj = {
+        body: commentFromApi.body,
+        votes: commentFromApi.votes,
+        author: commentFromApi.author,
+        created_at: commentFromApi.created_at,
+      };
+      setComments((currComments) => {
+        return [...currComments, newCommentObj];
+      });
+    });
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Post a new comment...
-        <input
-          type="text"
-          name="new-comment"
-          value={newComment}
-          onChange={(event) => setNewComment(event.target.value)}
-        />
-      </label>
-      <button type="submit">Post comment</button>
-    </form>
-  );
+    setNewComment("");
+    setHasPosted(true);
+  };
+  if (!hasPosted) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            id="commentbox"
+            name="new-comment"
+            placeholder="Enter your comment here..."
+            value={newComment}
+            onChange={(event) => setNewComment(event.target.value)}
+          />
+        </label>
+        <button type="submit">Post comment</button>
+      </form>
+    );
+  }
+
+  return <p>Please wait</p>;
 };
 
 export default AddComment;
